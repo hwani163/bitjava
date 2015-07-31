00faee9d5deaede873fedce523f0a87a921c2f8d
user <user@user02>
2015-07-31 16:01:26 +0900

    버전업! Commit Test

--- java01/src/test/ok/haha/Hello3.java
+++ java01/src/test/ok/haha/Hello3.java
@@ -5,6 +5,8 @@ public class Hello3 {
   public static void main(String[] args) {
     System.out.println("test..ok");
     System.out.println("형상관리를 테스트 하기위한 라인 이빈다.");
+    
+    System.out.println("안녕하세요 버전 업!!");
 
   }
 
